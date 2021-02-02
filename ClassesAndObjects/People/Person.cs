7071be9b88ee5f9be4c6597fdb0809d073a28982
Shriv852 <43151504+Shriv852@users.noreply.github.com>
2021-02-02 10:37:59 -0700

    Add files via upload

---
+++ ClassesAndObjects/People/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndObjects.People //'.People' links the program into another namespace (another folder where the 'person' script is in)
+{
+    public class Person
+    {
+        public string Name;
+        public int age;
+        public bool HasPet;
+
+        public void Greeting()
+        {
+            Console.WriteLine("Hey! My name is " + Name + " and I am " + age + " years old.");
+        }
+    }
+
+}
---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ClassesAndObjects.People;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(); //Instantiating a class so that the program would run
+            person.Name = "Lewis";
+            person.age = 29;
+            person.HasPet = true;
+
+            person.Greeting();
+        }
+    }
+}
